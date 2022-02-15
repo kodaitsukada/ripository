9981d0807597c3be11a90794ad31be5c52b05516
kodaitsukada <99584770+kodaitsukada@users.noreply.github.com>
2022-02-15 18:08:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+import * as platform from "https://cdn.skypack.dev/platform@1.3.6";
+import textillate from "https://cdn.skypack.dev/textillate@0.4.1";
+import $ from "https://cdn.skypack.dev/jquery@3.6.0";
+import * as platform from "https://cdn.skypack.dev/platform@1.3.6";
+
+const se = new Audio("https://otologic.jp/sounds/se/pre/Camera-Phone03-2.mp3");
+se.play();
+
+let c = 604800
+
+setInterval(() => {
+  $("#hour").text(Math.floor(c / 3600));
+  $("#min").text(Math.floor((c % 3600) / 60));
+  $("#sec").text(Math.floor(c % 60));
+  c = c - 1;
+}, 1000)
+
+$("#date").text(new Date().toLocaleString());
+
+$("#user").text(platform.description);
